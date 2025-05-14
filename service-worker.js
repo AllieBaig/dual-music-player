1322d98f2d5d21a9dce1e2042b0fad4b3a862043
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-14 13:55:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
-// service worker content
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('dual-music-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js',
+        '/playlist.js',
+        '/badges.js',
+        '/auth.js',
+        '/white_noise.mp3',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
