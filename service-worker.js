779c1d86288484020b0e810ac9466b4e0987e8e1
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-14 11:32:36 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,22 @@
-
-self.addEventListener("install", e => {
-  console.log("Service Worker installed");
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('dual-music-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js',
+        '/white_noise.mp3',
+        '/manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  // Basic fetch handler
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
